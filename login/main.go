790979a911bfd3ad5def55191616d0e571ed8039
fwijayatoped <ferianto.wijaya@tokopedia.com>
2022-09-14 19:18:51 +0700

    chore(login): change little config

--- login/main.go
+++ login/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("ini adalah kodingan login")
+	fmt.Println("ini adalah kodingan login yang sudah diubah engineer 2")
 	fmt.Println("ini adalah kodingan login kedua")
 
 }
