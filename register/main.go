7a3ae146fe4bab07cda0bc67b288568b6c7b6e34
fwijayatoped <ferianto.wijaya@tokopedia.com>
2022-09-15 16:25:32 +0700

    perf(register): improve register page

--- register/main.go
+++ register/main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("ini register page kita made by feri")
+	fmt.Println("ini register page kita made by feri improved by engineer b")
 	fmt.Println("penambahan baru")
 }
