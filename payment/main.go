29437e39e476adbd7761a7c9cfed7f92421caab5
fwijayatoped <ferianto.wijaya@tokopedia.com>
2022-09-15 19:19:31 +0700

    perf(payment): improve payment page

--- payment/main.go
+++ payment/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("this is payment page made by feri after fix")
+	fmt.Println("this is payment page made by feri after fix improved by engineer b")
 	fmt.Println("additional code")
 	fmt.Println("tadi ada yang kurang")
 }
